2aaccd86a53ecba5326a7c79ab28e5819fb8d5e7
Paty-power <paty258.34@gmail.com>
2025-05-01 10:46:13 -0300

    Site

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Delícias Gourmet</h1>
+      <p>Bem-vindo ao site da confeitaria!</p>
+    </div>
+  )
+}
+
+export default App
